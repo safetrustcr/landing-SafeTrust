a2f3d6104ab2c496795cb882230490eb7ca96cb3
Shantel peters. <Elizabethogunrinde2015@gmail.com>
2025-02-23 20:00:08 +0000

    feat: disover page

---
+++ src/app/discover/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/sections/hero";
+import Features from "@/components/sections/features";
+
+
+export default function Discover() {
+  return (
+    <main>
+      <Hero />
+      <Features />
+    </main>
+  );
+}
