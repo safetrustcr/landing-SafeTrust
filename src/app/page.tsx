c2e1fa1aa7a807af73c8e82731e56d8da16f16cb
kimcascante <kiimcm9@gmail.com>
2025-02-23 20:56:01 -0600

    Implement SafeTrust footer with shadcn/ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import HeroSection from "@/components/HeroSection";
 import FaqSection from "@/components/FaqSection";
+import { Footer } from "@/components/FooterSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FaqSection />
+      <Footer />
     </>
   );
 }
