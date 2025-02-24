14514d2fb76c22db9fd66fb67028163c33d972f6
Shantel peters. <Elizabethogunrinde2015@gmail.com>
2025-02-24 09:06:24 +0000

    apply changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import HeroSection from "@/components/HeroSection";
 import FaqSection from "@/components/FaqSection";
+import Discover from "@/components/DiscoverSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FaqSection />
+      <Discover />
     </>
   );
 }
