b9bd7d50187e0e03a1d29a59b3d374448d40dd64
Stepcu <69991785+Stepcu@users.noreply.github.com>
2025-02-26 17:59:17 -0600

    Resolving conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,20 @@
 
 import HeroSection from "@/components/HeroSection";
 import FaqSection from "@/components/FaqSection";
-import TransactionTiers from "@/components/PriceSection"
+import TransactionTiers from "@/components/PriceSection";
+import Discover from "@/components/DiscoverSection";
+import { Footer } from "@/components/FooterSection";
+import TestimonialSection from "@/components/TestimonialSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <TestimonialSection />
+      <Discover />
       <FaqSection />
       <TransactionTiers/>
-    </>
+      <Footer />
+      </>
   );
 }
