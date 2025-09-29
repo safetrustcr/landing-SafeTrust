5ef80ff95371d67c5c4f67c349acba617b744df7
Renzo Barcos <renzoespinosa.b@gmail.com>
2025-09-29 19:19:41 -0300

    feat: add export barrel for error components

---
+++ src/components/error/index.ts
@@ -0,0 +1,7 @@
+// Error Components Export Barrel
+export { default as ErrorPage } from './ErrorPage'
+export { default as NetworkError } from './NetworkError'
+export { default as ErrorBoundary } from './ErrorBoundary'
+
+// Re-export types
+export type { ErrorPageProps } from './ErrorPage'
